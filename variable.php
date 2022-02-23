01fa2366e5d0cdbead5a79e2ae5453e88c790803
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-02-24 01:36:41 +0300

    --- Auto-Git Commit ---

--- variable.php
+++ variable.php
@@ -13,3 +13,51 @@ $favorite_food = "Red curry with eggplant, green beans, and peanuts";
 echo $favorite_food;
 
 // Prints: Red curry with eggplant, green beans, and peanuts
+
+//calculations
+$dog_name = "Tadpole";
+echo "My dog is named " . $dog_name;
+
+// Prints: My dog is named Tadpole
+
+$dog_name = "Tadpole";
+$favorite_food = "salmon";
+$color = "brown";
+
+
+echo "I have a " . $color . " dog named " . $dog_name . " and her favorite food is " . $favorite_food . ".";
+// Prints: I have a brown dog named Tadpole and her favorite food is salmon.
+
+
+$noun = "programmer";
+$adjective = "serious";
+$verb = "lern";
+
+echo "The world's most beloved  $noun was very $adjective and loved to $verb every single day.";
+
+$toy = "frisbee";
+echo "Alex likes playing with $toys";
+
+/ Fill in the blanks in the code below:
+  $noun = "helicopter";
+  $adjective = "powerful";
+  $verb = "scream";
+
+  echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+
+
+//Fix the code below and uncomment it:
+
+ echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
+
+
+ // reassignement of a variable
+ $favorite_food = "Red curry with eggplant";
+echo $favorite_food; // Prints: Red curry with eggplant
+ 
+// $favorite_food = "Red curry with eggplant";
+echo $favorite_food; // Prints: Red curry with eggplant
+ 
+// Reassign the value of $favorite_food to a new string
+$favorite_food = "Pizza"; 
+echo $favorite_food; // Prints: Pizza
