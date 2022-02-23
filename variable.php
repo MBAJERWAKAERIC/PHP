adc7849f90939389e8168a33b03cdfe3ad18a459
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-02-24 01:01:24 +0300

    Update variable.php

--- variable.php
+++ variable.php
@@ -1,9 +1,15 @@
 <?php
 // Write your code below:
- $var1 = "Bob";
+$var1 = "Bob";
 echo $var1;
 // var1 holds the value "Bob"
- 
+
+
 $var1 = "John";
 echo $var1;
 // var1 now holds the value "John"
+
+$favorite_food = "Red curry with eggplant, green beans, and peanuts";
+echo $favorite_food;
+
+// Prints: Red curry with eggplant, green beans, and peanuts
