cd4170666a8e4d574dde48722c42fce4de38bcf5
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-02-24 03:42:37 +0300

    --- Auto-Git Commit ---

--- variable.php
+++ variable.php
@@ -92,3 +92,16 @@ echo "\nBut I'll always have a special place in my heart for $old_favorite.";
   // Don't change the line below
   echo "\nYour order is: $very_bad_unclear_name.";
 
+  echo "Hello, Learner!";
+
+  echo "\nWe hope you enjoyed this lesson.";
+  
+echo "\nAre you excited?" . "\nTo learn more?";
+  
+$favorite_language = "PHP";
+
+$message = "$favorite_language is by far our favorite language.";
+
+$message .= " It's yours now too, right?";
+  
+echo "\n" . $message . " Right?!";  
