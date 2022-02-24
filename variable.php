f9fff9480f1e758e7e229467bd888ab0ff1128bc
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-02-24 03:09:41 +0300

    Update variable.php

--- variable.php
+++ variable.php
@@ -77,3 +77,18 @@ echo "\nBut now my favorite is $movie.";
 
 // Add a statement below:
 echo "\nBut I'll always have a special place in my heart for $old_favorite.";
+
+
+/* Imagine a lot of code here */  
+  $very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+  $order =& $very_bad_unclear_name; 
+
+  $order .= ", 1 cheeseburger";
+
+  $order .= ", 3 side salads";
+    
+  // Don't change the line below
+  echo "\nYour order is: $very_bad_unclear_name.";
+
