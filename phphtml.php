8a2aa051341b64265fbd7167e43de112b2155c0c
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-02-28 03:34:44 +0300

    Create phphtml.php

---
+++ phphtml.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variables and strings</title>
+</head>
+
+<body>
+    <?php
+        echo "<h1> PHP AND HTML </h1>";
+        echo "<hr>"
+        echo "<h2>it's intersting</h2>"
+
+
+?>
+</body>
+
+</html>
