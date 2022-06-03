d8aa6625a12a38c68d2a3bb4f82b0101e0a9c506
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-06-03 05:23:59 +0300

    registration form

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+    $conn = mysqli_connect('localhost', 'root', '', 'eric') or die("connection failed:" . mysqli_connect_error());
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $bgroup = $_POST['bgroup'];
+
+        $sql = "INSERT INTO 'users' ('name', 'email', 'phone', 'bgroup') VALUES ('$name', '$email', '$phone', '$bgroup')";
+
+        $query = mysqli_query($conn, $sql);
+        if ($query) {
+            echo 'Entry Successful';
+
+        } else {
+            echo 'Error occured';
+        }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -7,8 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
+    <h1>Blood donation camp</h1>
 
-<body>
+<body bgcolor="FBB917">
+    <div> <h2>registration form</h2></div>
+    <form action='connect.php' method='post'>
+        <Label for="user"> Name:</Label><br/>
+        <input type="text" name='name' id='name' required/> <br/> <br/>
+
+        <Label for="email"> Email:</Label><br/>
+        <input type="text" name='email' id='email' required/> <br/> <br/>
+
+        <Label for="phone"> phone:</Label><br/>
+        <input type="text" name='phone' id='phone' required/> <br/> <br/>
+
+        <Label for="bgroup"> blood group:</Label><br/>
+        <input type="text" name='bgroup' id='bgroup' required/> <br/> <br/>
+
+        <input type='submit' name='submit' id="submit"/>
+    </form>
 
 </body>
 
